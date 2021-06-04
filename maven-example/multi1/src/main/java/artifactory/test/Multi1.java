587bb2aefdf2cfb0ccebead9deaa56c3805aabd5
josehui <cshuihui@outlook.com>
2021-06-04 13:44:12 +0800

    JME-1 - test issue

--- maven-example/multi1/src/main/java/artifactory/test/Multi1.java
+++ maven-example/multi1/src/main/java/artifactory/test/Multi1.java
@@ -3,7 +3,7 @@ package artifactory.test;
 /**
  * Hello world!
  */
-//  test issue
+//  test issue 2
 public class Multi1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
